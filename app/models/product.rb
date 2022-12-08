f54918fe6b6404d67d937ccb96b1e01fc9a10d52
Darrick Smith <smithwebdd@gmail.com>
2022-12-07 21:23:52 -0500

    Created product scaffold, updated minitests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,21 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :integer          not null, primary key
+#  description :text
+#  image_url   :string
+#  price       :decimal(8, 2)
+#  title       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Product < ApplicationRecord
+  validates :title, :description, :image_url, presence: true
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+    with: /\.(gif|jpg|png)\z/i,
+    message: 'must be a URL for GIF, JPG, or PNG image!'
+  }
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 end
